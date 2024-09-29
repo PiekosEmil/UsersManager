001e259c5f2edf404c4275ea2e9336c5dbf5eafd
Emil Piękoś <piekosemil@gmail.com>
2024-09-30 01:04:21 +0200

    Initial commit

---
+++ src/main/java/com/emilpiekos/usersmanager/UsersManagerApplication.java
@@ -0,0 +1,13 @@
+package com.emilpiekos.usersmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersManagerApplication.class, args);
+	}
+
+}
